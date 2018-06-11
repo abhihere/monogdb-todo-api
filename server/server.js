fa259441f29cfba1e25a4989469fd63ab058221d
abhihere <abhijeetraut58@gmail.com>
2018-06-11 22:22:06 +0530

    Basic field validation

--- server/server.js
+++ server/server.js
@@ -5,18 +5,22 @@ mongoose.connect('mongodb://localhost:27017/Todo');
 
 var Todo = mongoose.model('Task',{
   text: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
-//
 var newTodo = new Todo({
-  text: 'Go to sleep'
+  text: 'Swimming'
 });
 
 newTodo.save().then((result)=>{
@@ -36,3 +40,23 @@ otherTodo.save().then((result)=>{
 }, (err)=>{
   console.log(err);
 })
+
+var User = mongoose.model('Users',{
+  email: {
+    type: String,
+    trim: true,
+    minlength: 1,
+    required: true
+  }
+});
+
+var user1 = new User({
+  email: 'abhijeetraut@gmail.com'
+});
+
+user1.save().then((result)=>{
+  console.log(result);
+}, (err)=>{
+  console.log(err);
+  console.log('failed');
+});
